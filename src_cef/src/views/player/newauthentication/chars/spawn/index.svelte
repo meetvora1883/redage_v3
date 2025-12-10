88dab788f697aef1b61ff0b6330907d1e1b2ab5e
meetvora1883 <meetvora1883@gmail.com>
2025-12-10 17:03:14 +0530

    Update index.svelte

--- src_cef/src/views/player/newauthentication/chars/spawn/index.svelte
+++ src_cef/src/views/player/newauthentication/chars/spawn/index.svelte
@@ -4,40 +4,38 @@
     import fraction from 'json/fraction.js'
 
     import Spawn from './spawn.svelte'
-
-
 </script>
 
 <Spawn {...$selectChar.Data} isBan={$selectChar.ban} />
 
 <div class="auth__stats">
     <div class="auth__stat">
-        <div class="gray">Денег на руках</div>
+        <div class="gray">Cash on hand</div>
         <div>${format("money", $selectChar.Data.Money)}</div>
     </div>
     <div class="auth__stat">
-        <div class="gray">Денег на счету</div>
+        <div class="gray">Money in bank</div>
         <div>${format("money", $selectChar.Data.BankMoney)}</div>
     </div>
     <div class="auth__stat">
-        <div class="gray">Фракция</div>
+        <div class="gray">Faction</div>
         <div>{fraction[$selectChar.Data.FractionID]}</div>
     </div>
     {#if $selectChar.ban && $selectChar.ban.Reason}
         <div class="auth__stat">
-            <div class="gray">Причина</div>
+            <div class="gray">Reason</div>
             <div>{$selectChar.ban.Reason}</div>
         </div>
         <div class="auth__stat">
-            <div class="gray">Забанил</div>
+            <div class="gray">Banned by</div>
             <div>{$selectChar.ban.Admin}</div>
         </div>
         <div class="auth__stat">
-            <div class="gray">Дата бана</div>
+            <div class="gray">Ban date</div>
             <div>{$selectChar.ban.Time}</div>
         </div>
         <div class="auth__stat">
-            <div class="gray">Дата разбана</div>
+            <div class="gray">Unban date</div>
             <div>{$selectChar.ban.Until}</div>
         </div>
     {/if}
