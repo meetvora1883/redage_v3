3112a084140985e5b4166f8bb4b9cbfe503e32de
meetvora1883 <meetvora1883@gmail.com>
2025-12-10 15:08:32 +0530

    Update index.svelte

--- src_cef/src/views/player/newauthentication/start/registration/index.svelte
+++ src_cef/src/views/player/newauthentication/start/registration/index.svelte
@@ -33,13 +33,13 @@
 <div class="main">
     <div class="main__box">
         <div class="main__forms">
-            <InputCustom updateLang={(lang) => language = lang} setValue={(value) => regusername = value} value={regusername} placeholder={translateText('player2', 'Логин..')} type="text" icon="auth-user"/>
-            <InputCustom updateLang={(lang) => language = lang} setValue={(value) => regemail = value} value={regemail} placeholder={translateText('player2', 'Почта..')} type="email" icon="auth-mail"/>
-            <InputCustom updateLang={(lang) => language = lang} setValue={(value) => regpass1 = value} value={regpass1} placeholder={translateText('player2', 'Пароль..')} type="password" icon="auth-lock"/>
-            <InputCustom updateLang={(lang) => language = lang} setValue={(value) => regpass2 = value} value={regpass2} placeholder={translateText('player2', 'Повторите пароль..')} type="password" icon="auth-pass-repeat"/>
-            <InputCustom updateLang={(lang) => language = lang} setValue={(value) => regpromo = value} value={regpromo} placeholder={translateText('player2', 'Промокод..')} type="text" icon="auth-gift"/>
+            <InputCustom updateLang={(lang) => language = lang} setValue={(value) => regusername = value} value={regusername} placeholder={translateText('player2', 'Login..')} type="text" icon="auth-user"/>
+            <InputCustom updateLang={(lang) => language = lang} setValue={(value) => regemail = value} value={regemail} placeholder={translateText('player2', 'E-mail..')} type="email" icon="auth-mail"/>
+            <InputCustom updateLang={(lang) => language = lang} setValue={(value) => regpass1 = value} value={regpass1} placeholder={translateText('player2', 'Password..')} type="password" icon="auth-lock"/>
+            <InputCustom updateLang={(lang) => language = lang} setValue={(value) => regpass2 = value} value={regpass2} placeholder={translateText('player2', 'Repeat password..')} type="password" icon="auth-pass-repeat"/>
+            <InputCustom updateLang={(lang) => language = lang} setValue={(value) => regpromo = value} value={regpromo} placeholder={translateText('player2', 'Promo code..')} type="text" icon="auth-gift"/>
             <!--<div class="main__buttons_box main__buttons_registration">
-                <InputCustom setValue={(value) => regpromo = value} value={regpromo} placeholder="Промокод.." type="text" icon="auth-gift"/>
+                <InputCustom setValue={(value) => regpromo = value} value={regpromo} placeholder="Promo code..." type="text" icon="auth-gift"/>
                 <div class="main__button main_button_size_small">
                     <div class="main__button_top box-center">
                         <span class="auth-lock"/>
@@ -51,7 +51,7 @@
             </div>-->
             <div class="box-flex">
                 <div class="main__button main_button_size_large" on:click={onRegister}>
-                    <div class="main__button_left box-center">{translateText('player2', 'Создать')}</div>
+                    <div class="main__button_left box-center">{translateText('player2', 'Create')}</div>
                     <div class="main__button_right box-center">
                         <div class="main__button_square box-center">
                             <span class="auth-arrow"/>
